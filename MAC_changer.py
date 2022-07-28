b56774826fc6c59f11baa4f833f92b26528f9b74
Anton Illenberger <51234797+GidroN@users.noreply.github.com>
2022-07-28 14:04:21 +0400

    Update MAC_changer.py

--- MAC_changer.py
+++ MAC_changer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from random import randint
+import time as _time
 import subprocess
 import optparse 
 import re
@@ -8,12 +9,21 @@ import re
 def get_arguments():
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address.")
-	parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+	parser.add_option("-m", "--mac", dest="new_mac", help="[OPTIONAL] - New MAC address. if not MAC address -> generate random MAC.")
+	parser.add_option("-t", "--time", dest='gap', help="[OPTIONAL] - The MAC address will changed every time. Write it like (1s - 1 second, 5m - 5 minutes, 2h - 2 hours)")
 	(options, arguments) = parser.parse_args()
+	gap = options.gap
 	if not options.interface:
 		parser.error("[-] Please specify an interface, use --help for more info.")
+	if gap:
+		if 's' in gap:
+			options.gap = int(gap[:-1])
+		elif 'm' in gap:
+			options.gap = int(gap[:-1]) * 60
+		elif 'h' in gap:
+			options.gap = int(gap[:-1]) * 3600    
 	elif not options.new_mac:
-		options.new_mac = generate_mac()
+		options.new_mac = generate_mac()			
 	return options
 
 
@@ -49,6 +59,13 @@ def find_mac(interface):
 
 def main():
 	options = get_arguments()
+	if options.gap:
+		subprocess.call('clear', shell=True)
+		print("[!] To stop the process press 'CTRL + C'")
+		time = options.gap
+		while True:
+			change_mac(options.interface, generate_mac())
+			_time.sleep(time)
 	if find_mac(options.interface) == options.new_mac:
 		parser = optparse.OptionParser()
 		parser.error("[-] You already have this MAC address.")
